a84dc05a3c6696133a29c2355dd49899a6171832
Patrick Liang <pl204511194@yahoo.com>
2022-01-10 12:15:38 -0500

    commit tree 27e30aa4bd9e5e295d78612422b5d881a0d1669e

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let blockValue = Math.abs(someValue-42);
+    return blockValue;
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(someValue1, someValue2) {
+    return Math.abs((distanceFromHqInFeet(someValue1)-distanceFromHqInFeet(someValue2)));
+}
+
+function calculatesFarePrice(someValue1, someValue2) {
+    let result = 0;
+    if (distanceTravelledInFeet(someValue1, someValue2)>400 && distanceTravelledInFeet(someValue1, someValue2) <=2000) {
+        return Math.abs((distanceTravelledInFeet(someValue1, someValue2) -400 )* 0.02);
+    } else if (distanceTravelledInFeet(someValue1, someValue2) > 2000 && distanceTravelledInFeet(someValue1, someValue2) <=2500){
+        result = 25;
+        return result;
+    } else if (distanceTravelledInFeet(someValue1, someValue2) <= 400) {
+        return result;
+    } else {
+        return "cannot travel that far"
+    }
+}
